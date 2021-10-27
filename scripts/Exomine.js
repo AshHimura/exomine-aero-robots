6e59b233d9b385699a5313647fbecc07038f6e09
shelbyrossi <rossi.shelby@gmail.com>
2021-10-27 11:27:21 -0500

    task12

--- scripts/Exomine.js
+++ scripts/Exomine.js
@@ -1,1 +1,23 @@
-//Enables mineral purchase button event and exports html to be displayed
+//Enables mineral purchase button event and exports html to be displayedimport { DiamondSizes } from "./DiamondSizes.js"
+
+
+
+
+export const Exomine = () => {
+    return `
+        <h1Solar System Mining Marketplace</h1>
+
+        <article class="choices">
+            <section class="choices__governor options">
+                <h2>Choose a governor</h2>
+                ${governorChoice()}
+            </section>
+            <section class="choices__facility options">
+                <h2>Facility</h2>
+                
+            </section>
+           </article>
+
+       
+    `
+}
