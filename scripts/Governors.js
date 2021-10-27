0cf93f8a2a3c3c00df9d20d413a292998e93f0fc
shelbyrossi <rossi.shelby@gmail.com>
2021-10-27 13:08:47 -0500

    htmlfunctionforgov

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -1,1 +1,32 @@
-//Importing governor data from database, pairs to respective colony and/or handles active status event
+//Importing governor data from database, pairs to respective colony and/or handles active status eventdocument.addEventListener(
+import { setGovernors } from "./database.js"
+
+
+
+
+// a change event listener that invokes the SetGovernors function - 
+// "change",
+//     (event) => {
+//         if (event.target.name === "governor") {
+//             setGovernors(parseInt(event.target.value))
+//         }
+//     }
+// )
+
+
+export const governorChoice = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    //  the .map() method iterates the array and invokes teh function you define
+    const listItems = governor.map(governor=> {
+        return `<li>
+            <input type="radio" name="governor" value="${governor.id}" /> ${governor.name}
+        </li>`
+    })
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
